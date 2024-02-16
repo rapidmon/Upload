4ee903a207353ef04ee168dd864f9d0e212a9e2d
rapidmon <ehhwll@hanmail.net>
2024-02-16 18:07:13 +0900

    twitter api v2 설치 및 라이브러리에 맞게 js 수정

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const AWS = require('aws-sdk');
-const Twitter = require('twitter');
+const { TwitterApi } = require('twitter-api-v2');
+
+// DynamoDB 클라이언트 설정
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-const twitterClient = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+// Twitter API 클라이언트 설정
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_CONSUMER_KEY,
+  appSecret: process.env.TWITTER_CONSUMER_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-exports.handler = async (event) => {
-    const now = new Date().toISOString();
-    const params = {
-        TableName: "ReservationNews",
-        FilterExpression: "ReservedTime <= :now",
-        ExpressionAttributeValues: { ":now": now }
-    };
+// 읽기 및 쓰기 가능한 Twitter 클라이언트
+const rwClient = twitterClient.readWrite;
 
-    try {
-        const data = await dynamoDB.scan(params).promise();
-        for (const item of data.Items) {
-            await twitterClient.post('statuses/update', { status: item.summary + " " + item.link });
-            console.log("Tweet posted:", item.summary);
-            // 게시된 트윗 삭제 또는 상태 업데이트 로직 추가
-        }
-    } catch (error) {
-        console.error("Error:", error);
+exports.handler = async (event) => {
+  const params = {
+    TableName: "ReservationNews",
+    // 필터링 조건 추가 가능
+  };
+
+  try {
+    // DynamoDB에서 데이터 스캔
+    const { Items } = await dynamoDB.scan(params).promise();
+    console.log(`Scanned items: ${Items.length}`);
+
+    // 현재 시간
+    const now = new Date();
+
+    for (const item of Items) {
+      // DynamoDB 항목의 예약된 시간을 Date 객체로 변환
+      const reservedTime = new Date(item.ReservedTime);
+
+      // 예약된 시간이 현재 시간보다 이전인 경우 트윗 게시
+      if (reservedTime <= now) {
+        const { data: createdTweet } = await rwClient.v2.tweet(item.summary + " " + item.link);
+        console.log("Tweet posted:", createdTweet);
+      }
     }
+  } catch (error) {
+    // 에러 로깅
+    console.error("Error:", error);
+  }
 };
