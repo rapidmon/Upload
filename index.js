86268445d1ee3d6aa3cf4548eef2b85f0cfa281f
rapidmon <ehhwll@hanmail.net>
2024-02-19 16:08:31 +0900

    facebook upload code 추가

--- index.js
+++ index.js
@@ -16,9 +16,20 @@ const twitterClient = new TwitterApi({
 const rwClient = twitterClient.readWrite;
 
 exports.handler = async (event) => {
+  let fetch;
+
+  // node-fetch 동적 임포트
+  try {
+    if (!fetch) {
+      fetch = (await import('node-fetch')).default;
+    }
+  } catch (error) {
+    console.error("Failed to dynamically import 'node-fetch':", error);
+    return;
+  }
+
   const params = {
     TableName: "ReservationNews",
-    // 필터링 조건 추가 가능
   };
 
   try {
@@ -33,10 +44,29 @@ exports.handler = async (event) => {
       // DynamoDB 항목의 예약된 시간을 Date 객체로 변환
       const reservedTime = new Date(item.ReservedTime);
 
-      // 예약된 시간이 현재 시간보다 이전인 경우 트윗 게시
+      // 예약된 시간이 현재 시간보다 이전인 경우 트윗과 Facebook 게시물 게시
       if (reservedTime <= now) {
+        // Twitter에 게시
         const { data: createdTweet } = await rwClient.v2.tweet(item.summary + " " + item.link);
         console.log("Tweet posted:", createdTweet);
+
+        // Facebook에 게시
+        const message = item.summary + " " + item.News;
+        const pageAccessToken = process.env.PAGE_ACCESS_TOKEN; // 페이지 액세스 토큰
+        const pageId = process.env.PAGE_ID; // 페이지 ID
+        const url = `https://graph.facebook.com/${pageId}/feed`;
+
+        const response = await fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            message: message,
+            access_token: pageAccessToken,
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const fbData = await response.json();
+        console.log("Facebook post created:", fbData);
       }
     }
   } catch (error) {
